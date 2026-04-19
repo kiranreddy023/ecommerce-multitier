381b9f22c56e6437125b34f188d036236055d83b
KIRAN KUMAR V <109744725+kiranreddy023@users.noreply.github.com>
2026-04-19 08:26:11 +0530

    Added home file

---
+++ frontend-app/src/main/java/com/ecommerce/frontend/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.ecommerce.frontend.controller;
+
+import com.ecommerce.frontend.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("products", service.getAllProducts());
+        return "index";
+    }
+}
